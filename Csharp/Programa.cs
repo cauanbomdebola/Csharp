098c043b117d98731cd6411d2209773bcbf6e3f4
cauanbomdebola <133662331+cauanbomdebola@users.noreply.github.com>
2024-06-03 23:09:02 -0300

    Add files via upload

---
+++ Csharp/Programa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Primeiro Exercício*/
+
+        /*int salarioPorHora = 35;
+        int horasTrabalhadas = 8;
+        int salarioDiario = salarioPorHora * horasTrabalhadas;
+        Console.WriteLine("O salário diário de Joaquim é: R$" + salarioDiario);*/
+
+        /*Segundo Exercício*/
+
+        /*int unidadesPorCaixa = 27;
+        int unidadesCompradas = 16;
+        int totalBombons = unidadesPorCaixa * unidadesCompradas;
+        Console.WriteLine("Pamela tem " + totalBombons + " bombons.");*/
+
+        /*Terceiro Exercício*/
+
+        /*int inteiro2 = 5;
+        int inteiro3 = 10;
+        int soma = inteiro2 + inteiro3;
+        Console.WriteLine("Digite dois números inteiros separados por espaço: " + soma);*/
+
+        /*Quarto Exercício*/
+
+        /*int garrafasPorCaixa = 20;
+        int totalGarrafasVendidas = 300;
+        int totalCaixasUsadas = totalGarrafasVendidas / garrafasPorCaixa;
+        Console.WriteLine("Foram usadas " + totalCaixasUsadas + " caixas.");*/
+
+        /*Quinto Exercício*/
+
+        /*double precoInstalacao = 300.00;
+        double precoLimpeza = 150.00;
+        double precoManutencao = 499.99;
+        double descontoPromocional = 50.00;
+
+        double totalComDesconto = precoInstalacao + precoLimpeza + precoManutencao - descontoPromocional;
+
+        Console.WriteLine("O cliente deverá pagar à empresa: R$" + totalComDesconto);*/
+    }
+}
+
